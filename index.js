b9ff27fa682a8f0dfa09f711e1e0f8f8c5435e8e
deveshkota <166916865+deveshkota@users.noreply.github.com>
2024-04-14 00:41:00 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Spotify");
 
 let songIndex = 0;
-let audioElement = new Audio('spotify/1.mp3');
+let audioElement = new Audio('spotify/spotify/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
